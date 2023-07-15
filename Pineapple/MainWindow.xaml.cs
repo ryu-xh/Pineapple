9489e52a3c0d225eaa618640bca0db138063d5bb
Ryuusei Kang <ryuuseikang@naver.com>
2023-07-16 00:03:35 +0900

    feature: oto 파일 캘리브레이션 추가

--- Pineapple/MainWindow.xaml.cs
+++ Pineapple/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -37,9 +39,11 @@ namespace Pineapple
         {
             Ko,
             Ja,
+            Zh,
             Unknown
         }
 
+        string zhList = "偁偄偆偊傫偍偽傃傇傋傏傖傘偉傚偪偩偱偂偳偅傆偀偋偑偓偖偘偛偼傂傊傎偠偐偒偔偗偙傑傒傓傔傕側偵偸偹偺傁傄傉傌傐傜傝傞傟傠偝偡偣偦偟偨偰偲償傢傪傗備傛偞偫偢偤偧偮僈僎僊僑僌";
         private Lang DetectJAKOString(string str)
         {
             foreach (char chr in str)
@@ -47,6 +51,11 @@ namespace Pineapple
                 if (chr >= '가' && chr < '힣')
                 {
                     return Lang.Ko;
+                }
+
+                if (zhList.IndexOf(chr) >= 0)
+                {
+                    return Lang.Zh;
                 }
 
                 if ((chr >= 0x3040 && chr <= 0x30FF) || (chr >= 0x4E00 && chr<= 0x9FBF))
@@ -58,8 +67,9 @@ namespace Pineapple
             return Lang.Unknown;
         }
 
-        Encoding ja = Encoding.GetEncoding(949);
-        Encoding ko = Encoding.GetEncoding(932);
+        Encoding ja = Encoding.GetEncoding(932);
+        Encoding ko = Encoding.GetEncoding(949);
+        Encoding zh = Encoding.GetEncoding("gb18030");
         private void RenameFile(string originDirectory, string file)
         {
             var fileName = file.Split('\\');
@@ -69,10 +79,13 @@ namespace Pineapple
             switch (DetectJAKOString(fileName[fileName.Length - 1]))
             {
                 case Lang.Ja:
-                    changedFileName = Convert(fileName[fileName.Length - 1], ja, ko);
+                    changedFileName = Convert(fileName[fileName.Length - 1], ko, ja);
                     break;
                 case Lang.Ko:
-                    changedFileName = Convert(fileName[fileName.Length - 1], ko, ja);
+                    changedFileName = Convert(fileName[fileName.Length - 1], ja, ko);
+                    break;
+                case Lang.Zh:
+                    changedFileName = Convert(fileName[fileName.Length - 1], ja, zh);
                     break;
                 case Lang.Unknown:
                     changedFileName = fileName[fileName.Length - 1];
@@ -93,25 +106,55 @@ namespace Pineapple
 
         private string Convert(string str, Encoding formatFrom, Encoding formatTo)
         {
-            Encoding shiftJIS = formatFrom;
-
             EncodingInfo[] encods = Encoding.GetEncodings();
 
             Encoding destEnc = Encoding.UTF8;
 
             Encoding enc = formatTo;
             byte[] sorceBytes = enc.GetBytes(str);
-            byte[] encBytes = Encoding.Convert(shiftJIS, destEnc, sorceBytes);
+            byte[] encBytes = Encoding.Convert(formatFrom, destEnc, sorceBytes);
 
             return destEnc.GetString(encBytes);
         }
 
-
-        private void ShowAd()
+        private Encoding DectectEncoding(string file)
         {
-            System.Diagnostics.Process.Start("https://pbs.twimg.com/media/FSLqrg7agAEEyT8?format=png&name=large");
+            string jaStrings = "あいうえんおばびぶべぼゃゅぇょちだでぃどぅふぁぉがぎぐげごはひへほじかきくけこまみむめもなにぬねのぱぴぷぺぽらりるれろさすせそしたてとわをやゆよざぢずぜぞつ";
+            using (StreamReader sr = new StreamReader(file, Encoding.UTF8, true))
+            {
+                string data = sr.ReadToEnd();
+                sr.Close();
+                for (int i = 0; i < jaStrings.Length; i++)
+                {
+                    if (data.Contains(jaStrings[i]))
+                        return Encoding.UTF8;
+                }
+            }
 
-            Progress_Text.Text = "완료";
+            using (StreamReader sr = new StreamReader(file, ko))
+            {
+                string data = sr.ReadToEnd();
+                sr.Close();
+                for (int i = 0; i< jaStrings.Length; i++)
+                {
+                    if (data.Contains(jaStrings[i]))
+                        return ko;
+                }
+            }
+
+            using (StreamReader sr = new StreamReader(file, zh))
+            {
+                string data = sr.ReadToEnd();
+                sr.Close();
+                for (int i = 0; i < jaStrings.Length; i++)
+                {
+                    if (data.Contains(jaStrings[i]))
+                        return zh;
+                }
+            }
+
+            return ja;
+
         }
 
         private void Progress(string[] folders)
@@ -120,30 +163,63 @@ namespace Pineapple
             {
                 string path = folder;
 
-                string[] files = Directory.GetFiles(path, "*.*");
-
-                foreach (var file in files)
+                if ((File.GetAttributes(path) & FileAttributes.Directory) == FileAttributes.Directory)
                 {
-                    RenameFile(path, file);
-                    System.Windows.Forms.Application.DoEvents();
-                }
+                    string[] files = Directory.GetFiles(path, "*.*");
 
-                DirectoryInfo di = new DirectoryInfo(path);
-                DirectoryInfo[] dirs = di.GetDirectories("*.*", SearchOption.AllDirectories);
-
-                foreach (DirectoryInfo d in dirs)
-                {
-                    string[] dirctoryFiles = Directory.GetFiles(d.FullName, "*.*");
-
-                    foreach (var file in dirctoryFiles)
+                    foreach (var file in files)
                     {
-                        RenameFile(d.FullName, file);
+                        RenameFile(path, file);
                         System.Windows.Forms.Application.DoEvents();
+                    }
+
+                    DirectoryInfo di = new DirectoryInfo(path);
+                    DirectoryInfo[] dirs = di.GetDirectories("*.*", SearchOption.AllDirectories);
+
+                    foreach (DirectoryInfo d in dirs)
+                    {
+                        string[] dirctoryFiles = Directory.GetFiles(d.FullName, "*.*");
+
+                        foreach (var file in dirctoryFiles)
+                        {
+                            RenameFile(d.FullName, file);
+                            System.Windows.Forms.Application.DoEvents();
+                        }
+                    }
+                } else
+                {
+                    if (path.EndsWith("\\oto.ini"))
+                    {
+                        Encoding enc = DectectEncoding(path);
+                        string contents;
+                        using (StreamReader sr = new StreamReader(path, enc, true))
+                        {
+                            enc = sr.CurrentEncoding;
+                            contents = sr.ReadToEnd();
+                            sr.Close();
+                        }
+
+                        using (StreamWriter sw = new StreamWriter(path, false, Encoding.GetEncoding(932)))
+                        {
+                            sw.WriteLine(contents);
+                            sw.Close();
+                            sw.Dispose();
+                        }
+
+                        Progress_Text.Text = "Calibration complete oto.ini";
                     }
                 }
 
+
                 ShowAd();
             }
+        }
+
+        private void ShowAd()
+        {
+            System.Diagnostics.Process.Start("https://pbs.twimg.com/media/FSLqrg7agAEEyT8?format=png&name=large");
+
+            Progress_Text.Text = "완료";
         }
 
         private void Grid_Drop(object sender, System.Windows.DragEventArgs e)
